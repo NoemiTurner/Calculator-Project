ea6cf48f4e133d038648baa2f28f6366e1ccfc50
Noemi Turner <nturner3@zagmail.gonzaga.edu>
2021-06-07 22:44:26 -0700

    First Commit for Calculator Project

---
+++ calculator.cpp
@@ -0,0 +1,9 @@
+//----------------------------------------------------------------------
+// Name: Noemi Turner
+// File: calculator.cpp
+// Date: Summer 2021
+// Desc: Basic calculator project
+//----------------------------------------------------------------------
+
+#include "calculator.h"
+
---
+++ calculator.h
@@ -0,0 +1,23 @@
+//----------------------------------------------------------------------
+// Name: Noemi Turner
+// File: calculator.h
+// Date: Summer 2021
+// Desc: Basic calculator project
+//----------------------------------------------------------------------
+
+
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+class Calculator
+{
+public:
+    // default destructor
+    virtual ~Calculator() {};
+
+private:
+
+
+};
+
+#endif
